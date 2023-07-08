0e2efcc1a2c5d6335ca0170ad8110bb13ee7157a
Kirandeep <kirandeep1195@gmail.com>
2023-07-08 17:44:37 -0400

    added data in index file

--- index.js
+++ index.js
@@ -22,18 +22,34 @@ const questions = [
     {
         type : 'input',
         message: 'Enter color for shape',
-        name: 'shaorColor',
+        name: 'shapeColor',
     },
 ]
 
+
+
 function writeToFile(fileName, data) {
-    fs.writeFileSync(fileName, shape(data))
+    fs.writeFileSync(fileName, data)
 }
 function init() {
     inquirer.prompt(questions)
     .then( (data) => {
         console.log(data)
-        writeToFile("svg.logo",data)
+
+        let newShape;
+
+if(data.shape == 'Circle') {
+newShape = new Circle(data.text, data.textColor, data.shape, data.shapeColor)
+}
+
+ else if (data.shape === 'Triangle') {
+    newShape = new Triangle(data.text, data.textColor, data.shape, data.shapeColor)
+ }
+ else {
+    newShape = new Square(data.text, data.textColor, data.shape, data.shapeColor)
+ }
+console.log(newShape)
+        writeToFile("logo.svg",newShape.render())
     })
 }
 init()
